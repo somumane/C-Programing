5cb4c5c8224d82643b30e36137baabdd3b2ff793
somumane <106975847+somumane@users.noreply.github.com>
2023-10-14 20:52:23 +0530

    Basic Pointers

---
+++ Pointer.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main(){
+    int c;
+    static int d;
+    int a=10,b=20;
+    int *p,*q;
+    p=&a;
+    q=&b;
+    printf("The value of a=%d\n",a);
+    printf("The value of b=%d\n",b);
+    printf("The adress of a=%u\n",&a);
+    printf("The adress of b=%u\n",&b);
+    printf("The value of p=%d\n",p);// the p will be stored adress of a
+    printf("The value of q=%d\n",q);// the q will be store adrees of b
+    printf("The p pointing value is=%d\n",*p);//the * is indireaction operator
+    printf("The q pointing value is=%d\n",*q);
+    printf("The adress of p=%u\n",&p); //the pointers will be have adress 
+    printf("The adress of q=%u\n",&q);
+    printf("%d\n",c); //this is auto type its stores garbage value its store in RAM
+     printf("%d",d);//this is static type memory its also store in RAM
+}
---
+++ Varibales.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a=5;
+    float b=1.23;
+    char c='A';
+    printf("%d,%.1f,%c",a,b,c);// this represents of data type in o/p %d integer %f float %c char
+    return 0;
+}
